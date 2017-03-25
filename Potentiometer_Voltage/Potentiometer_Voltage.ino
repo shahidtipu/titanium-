f1008a1adebcca1caf9b40516f1ce580e6e3a300
shahidtipu <tipuiubat@gmail.com>
2017-03-25 15:39:34 +0600

    Add files via upload
    Arduino code for reading potentiometer

---
+++ Potentiometer_Voltage/Potentiometer_Voltage.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensorValue = analogRead(A0);
+  float voltage=sensorValue*(5.0/1023.0);
+  Serial.print(voltage);
+  Serial.print("  v");
+  Serial.println("");
+  delay(500);
+}
